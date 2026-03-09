6e7131bc12277d3d68d64bd34ba604fee0062022
Mitchell Wildman <mitchellwildman1@gmail.com>
2026-03-09 13:46:11 +1000

    Add JS version of open-next.config to satisfy build

---
+++ open-next.config.js
@@ -0,0 +1,9 @@
+/**
+ * OpenNext configuration (JavaScript) for Cloudflare deployment
+ * Using JS since build environment may not transpile TS config.
+ */
+
+module.exports = {
+  projectRoot: '.',
+  output: 'dist',
+};
